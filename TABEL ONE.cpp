0b473eafcef05c326573bc93833c7ad76335b934
ghost_flash <slamet.0409@students.amikom.ac.id>
2018-03-23 11:41:07 +0700

    tabel 1
    ghost_flash

---
+++ TABEL ONE.cpp
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color:GREEN;
+}
+</style>
+</head>
+<body>
+
+<h2>HTML Table</h2>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Position</th>
+    <th>Office</th>
+    <th>Date Start</th>
+    <th>Age</th>
+    <th>Salary</th>
+  </tr>
+  <tr>
+    <td>Anthony A. Gibbs</td>
+    <td>CEO</td>
+    <td>Practi-Plan Mapping</td>
+    <td>12/04/2004</td>
+    <td>24</td>
+    <td>$200.000</td>
+  </tr>
+  <tr>
+    <td>Roy J. Little</td>
+    <td>CEO</td>
+    <td>Macroserve</td>
+    <td>18/04/2007</td>
+    <td>24</td>
+    <td>$200.000</td>
+  </tr>
+  <tr>
+    <td>Roy . Little</td>
+    <td>CEO</td>
+    <td>Macrerve</td>
+    <td>18/04/2005</td>
+    <td>27</td>
+    <td>$20.000</td>
+  </tr>
+  <tr>
+    <td>Ro. Lit</td>
+    <td>CEO</td>
+    <td>Macro</td>
+    <td>18/04/2004</td>
+    <td>28</td>
+    <td>$2500.000</td>
+  </tr>
+  <tr>
+    <td> Little</td>
+    <td>CEO</td>
+    <td>roserve</td>
+    <td>18/04/2017</td>
+    <td>24</td>
+    <td>$200.000</td>
+  </tr>
+  <tr>
+    <td>Boy J. Litle</td>
+    <td>CEO</td>
+    <td>Marve</td>
+    <td>18/04/2017</td>
+    <td>24</td>
+    <td>$207.000</td>
+  </tr>
+   <tr>
+    <td>Voy J. Litle</td>
+    <td>CEO</td>
+    <td>MarUve</td>
+    <td>18/04/2019</td>
+    <td>24</td>
+    <td>$205.000</td>
+  </tr>
+   <tr>
+    <td>BTy J. Litle</td>
+    <td>CEO</td>
+    <td>Marve</td>
+    <td>1T8/04/2017</td>
+    <td>24</td>
+    <td>$207.000</td>
+  </tr>
+</table>
+
+</body>
+</html>
+
